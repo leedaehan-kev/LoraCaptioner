46a247fe32a1ed4cdc68fc8aa44ad678d64dcafa
Yejoon Lee <beeriver1@gmail.com>
2023-06-09 01:54:38 +0900

    Change default format of main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=attn-only
+#SBATCH --job-name=train
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=8
 #SBATCH --time=24:00:00
+#SBATCH --partition=rtx6000
 
 conda activate tl
-python main.py configs/attn-only.yaml --run_name server-64-attn-only
+python main.py configs/default.yaml --run_name server
